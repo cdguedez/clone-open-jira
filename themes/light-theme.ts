0cda211a3e8884b1d46ab5cc456b119ee10085cb
Carlos Guedez <cdgr.ing@gmail.com>
2023-11-14 19:32:56 -0400

    Add background, secondary, and error colors to light theme, and adjust typography sizes.

--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -1,7 +1,37 @@
 import { createTheme } from '@mui/material'
+import { blueGrey, red, lightBlue } from '@mui/material/colors'
 
 export const lightTheme = createTheme({
   palette: {
-    mode: 'light'
+    mode: 'light',
+    background: {
+      default: lightBlue[100]
+    },
+    secondary: {
+      main: blueGrey[300]
+    },
+    error: {
+      main: red[500]
+    }
+  },
+  typography: {
+    h1: {
+      fontSize: '4rem'
+    },
+    h2: {
+      fontSize: '3rem'
+    },
+    h3: {
+      fontSize: '2rem'
+    },
+    h4: {
+      fontSize: '1.5rem'
+    },
+    h5: {
+      fontSize: '1.25rem'
+    },
+    h6: {
+      fontSize: '1rem'
+    }
   }
 })
