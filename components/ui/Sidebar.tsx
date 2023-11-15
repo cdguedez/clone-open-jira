ffa8db89f8b7a44b6ae7257533bde8a09aec8820
Carlos Guedez <cdgr.ing@gmail.com>
2023-11-14 20:52:03 -0400

    Add menu items to Sidebar component

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,4 +1,12 @@
-import { Box, Drawer, Typography } from '@mui/material'
+import { Dns, People, PermMedia, Public } from '@mui/icons-material'
+import { Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material'
+
+const menuItems = [
+  { icon: <People />, label: 'Authentication' },
+  { icon: <Dns />, label: 'Database' },
+  { icon: <PermMedia />, label: 'Storage' },
+  { icon: <Public />, label: 'Hosting' }
+]
 
 export const Sidebar = () => {
   return (
@@ -7,11 +15,23 @@ export const Sidebar = () => {
       open={true}
       onClose={() => { console.log('CLOSE DRWAER') }}
     >
-      <Box sx={{ width: 300 }}>
+      <Box sx={{ width: 300 }} component='aside'>
         <Box sx={{ padding: '.5rem' }}>
           <Typography variant='h4'>
             Menú
           </Typography>
+          <List aria-label='menu'>
+            {
+              menuItems.map(({ icon, label }, index) => (
+                <ListItemButton component='a' key={index}>
+                  <ListItemIcon>
+                    { icon }
+                  </ListItemIcon>
+                  <ListItemText primary={label} />
+                </ListItemButton>
+              ))
+            }
+          </List>
         </Box>
       </Box>
     </Drawer>
