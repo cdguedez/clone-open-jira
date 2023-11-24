9e72a793413b6d8133786f01b2044b1e02d3dd35
Carlos Guedez <cdgr.ing@gmail.com>
2023-11-24 16:16:39 -0400

    Add grid layout and cards to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
-import { MainLayout } from '@/components/layouts'
-import { Typography } from '@mui/material'
+import { type NextPage } from 'next'
+import { Card, CardContent, CardHeader, Grid } from '@mui/material'
 
-export default function Home () {
+import { MainLayout } from '@/components/layouts'
+
+const Home: NextPage = () => {
   return (
       <MainLayout title='Home Open Jira'>
-        <Typography variant='h1' component='h1'>Home Page</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Card sx={{ height: 'calc(100vh - 100px)' }}>
+              <CardHeader title='Pendientes' />
+              <CardContent>
+                <p>En esta sección se mostrarán las tareas pendientes</p>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Card sx={{ height: 'calc(100vh - 100px)' }}>
+              <CardHeader title='En Desarrollo' />
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Card sx={{ height: 'calc(100vh - 100px)' }}>
+              <CardHeader title='Listas' />
+            </Card>
+          </Grid>
+        </Grid>
       </MainLayout>
   )
 }
+
+export default Home
