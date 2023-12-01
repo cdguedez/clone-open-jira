6dd0ef66c46f52e090c858792ec26146e2358604
Carlos Guedez <cdgr.ing@gmail.com>
2023-11-30 22:07:24 -0400

    Fix import path in EntryList component

--- components/ui/Entry/EntryList.tsx
+++ components/ui/Entry/EntryList.tsx
@@ -1,6 +1,6 @@
 import { List, Paper } from '@mui/material'
 import React, { useContext, useMemo } from 'react'
-import { EntryCard } from '..'
+import { EntryCard } from '../'
 import { EntriesContext } from '@/context/entries'
 import { type EntryStatus } from '@/interfaces'
 
@@ -11,13 +11,13 @@ interface EntryListProps {
 export const EntryList = ({ status }: EntryListProps) => {
   const { entries } = useContext(EntriesContext)
 
-  const entriesRender = useMemo(() => entries.filter(entry => entry.status === status), [entries])
+  const entriesRenderByStatus = useMemo(() => entries.filter(entry => entry.status === status), [entries])
 
   return (
     <div>
       <Paper sx={{ height: 'calc(100vh - 200px)', overflow: 'scroll', backgroundColor: 'transparent', padding: '2px 6px' }}>
         <List sx={{ opacity: 1 }}>
-          {entriesRender.map(entry => (
+          {entriesRenderByStatus.map(entry => (
             <EntryCard key={entry._id} entry={entry} />
           ))}
         </List>
